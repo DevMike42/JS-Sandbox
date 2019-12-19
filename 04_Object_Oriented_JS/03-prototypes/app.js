30546b568966818686212274352a2c7e1152fc09
Mike Rivera <michaelstephenrivera@gmail.com>
2019-12-18 18:07:35 -0800

    Prototypes

---
+++ 04_Object_Oriented_JS/03-prototypes/app.js
@@ -0,0 +1,52 @@
+// PROTOTYPES
+
+// Object.prototype
+// Person.prototype
+
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+  // this.calculateAge = function () {
+  //   const diff = Date.now() - this.birthday.getTime();
+  //   const ageDate = new Date(diff);
+  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
+  // }
+}
+
+// Calculate age
+Person.prototype.calculateAge = function () {
+  const diff = Date.now() - this.birthday.getTime();
+  const ageDate = new Date(diff);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+// Get full name
+Person.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`
+}
+
+// Gets Married
+Person.prototype.getsMarried = function (newLastName) {
+  this.lastName = newLastName;
+}
+
+const john = new Person('John', 'Doe', '8-12-90');
+const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+
+console.log(mary); // Look in console to view __proto__
+
+// calculateAge method can be used just as if it were inside the constructor function
+console.log(john.calculateAge());
+
+console.log(mary.getFullName());
+
+mary.getsMarried('Smith');
+
+console.log(mary.getFullName());
+
+// Accessing the hasOwnProperty method of the Object prototype
+console.log(mary.hasOwnProperty('firstName'));
+
+// It's not an actual property
+console.log(mary.hasOwnProperty('getFullName'));
