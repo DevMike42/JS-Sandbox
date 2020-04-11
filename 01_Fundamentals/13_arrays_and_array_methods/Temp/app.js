5259e909e18c1c7ed8eca137a2870a4a4692732a
Mike Rivera <michaelstephenrivera@gmail.com>
2020-04-10 17:49:05 -0700

    Working with Arrays

--- 01_Fundamentals/13_arrays_and_array_methods/Temp/app.js
+++ 01_Fundamentals/13_arrays_and_array_methods/Temp/app.js
@@ -11,27 +11,22 @@ const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
 
 // Array of Mixed data types
 const mixed = [22, 'Hello', true, undefined, null, { a: 1, b: 1 }, new Date()];
-console.log(mixed);
+// console.log(mixed);
 
 let val;
 
-
-$('a').on('click', function (e) {
-  const btnId = e.target.id
-  console.log(btnId);
-
-  switch (btnId) {
-    case 'arr-length':
-      findArrLength();
-      break;
-
-    default:
-      break;
-  }
+$(document).ready(() => {
+  $('#array1').text(numbers);
+  $('#array2').text(numbers2);
 });
 
 
+// ==========================================
+// ACCESSING ARRAY DATA
 
+const refreshArrayUI = (arrUiId, arr) => {
+  $(arrUiId).text(arr);
+};
 
 // Get Array length
 const findArrLength = () => {
@@ -39,134 +34,152 @@ const findArrLength = () => {
   console.log(val);
 };
 
-// ---------------------------------------
-
 // Check if is Array
-val = Array.isArray(numbers);
-
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
+const checkIfIsArray = () => {
+  val = Array.isArray(numbers);
+  console.log(val);
+};
 
 // Get single value 
-val = numbers[3];
-
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
-
-// Insert into Array
-numbers[2] = 100;
-
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
+const getSingleValue = () => {
+  val = numbers[3];
+  console.log(val);
+};
 
 // Find index of value
-val = numbers.indexOf(36);
-
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
+const findIndexOf = () => {
+  val = numbers.indexOf(36);
+  console.log(val);
+};
 
 // ==========================================
 // MUTATING ARRAYS
 
+// Insert into Array
+const insertInto = () => {
+  numbers2[2] = 100;
+  refreshArrayUI('#array2', numbers2);
+  console.log(numbers2);
+};
+
 // Add on to end
-numbers.push(250);
-
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
-
-// Add on to front
-numbers.unshift(120);
-
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
+const addToEnd = () => {
+  numbers2.push(250);
+  refreshArrayUI('#array2', numbers2);
+  console.log(numbers2);
+};
 
 // Take off from end
-numbers.pop();
+const removeFromEnd = () => {
+  numbers2.pop();
+  refreshArrayUI('#array2', numbers2);
+  console.log(numbers2);
+};
 
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
+// Add on to front
+const addToFront = () => {
+  numbers2.unshift(120);
+  refreshArrayUI('#array2', numbers2);
+  console.log(numbers2);
+};
 
 // Take off from front
-numbers.shift();
-
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
+const removeFromFront = () => {
+  numbers2.shift();
+  refreshArrayUI('#array2', numbers2);
+  console.log(numbers2);
+};
 
 // Splice values
-numbers.splice(1, 1); // Parameters = (starting index, ending index)
+const removeSingleIndex = () => {
+  numbers2.splice(3, 1); // Parameters = (starting index, delete count)
+  refreshArrayUI('#array2', numbers2);
+  console.log(numbers2);
+};
 
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
-// ---------------------------------------
+const removeMultipleIndexes = () => {
+  numbers2.splice(2); // Parameters = (starting index) removed remaining if no 2nd param.
+  refreshArrayUI('#array2', numbers2);
+  console.log(numbers2);
+};
 
 // Reverse
-numbers.reverse();
+// numbers.reverse();
 
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
+// console.log(numbers);
+// console.log(val);
+// console.log('--------------------');
 // ---------------------------------------
 
 // Concatenate array
-val = numbers.concat(numbers2);
+// val = numbers.concat(numbers2);
 
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
+// console.log(numbers);
+// console.log(val);
+// console.log('--------------------');
 // ---------------------------------------
 
 // SORT
-val = fruit.sort(); // Sorts it in Alphabetical order
+// val = fruit.sort(); // Sorts it in Alphabetical order
 // console.log(numbers);
-console.log(val);
+// console.log(val);
 
-val = numbers.sort(); // Notice it sorts by the first number
+// val = numbers.sort(); // Notice it sorts by the first number
 // console.log(numbers);
-console.log(val);
+// console.log(val);
 
 // Use the "compare function"
-val = numbers.sort(function (x, y) {
-  return x - y;
-});
+// val = numbers.sort(function (x, y) {
+//   return x - y;
+// });
 // console.log(numbers);
-console.log(val);
+// console.log(val);
 
 // Reverse sort
-val = numbers.sort(function (x, y) {
-  return y - x;
-});
+// val = numbers.sort(function (x, y) {
+//   return y - x;
+// });
 // console.log(numbers);
-console.log(val);
+// console.log(val);
 
-console.log('--------------------');
+// console.log('--------------------');
 // ---------------------------------------
 
 // Find
-function under50(num) {
-  return num < 50;
-}
+// function under50(num) {
+//   return num < 50;
+// }
 
-val = numbers.find(under50);
+// val = numbers.find(under50);
 
-console.log(numbers);
-console.log(val);
-console.log('--------------------');
+// console.log(numbers);
+// console.log(val);
+// console.log('--------------------');
 // ---------------------------------------
 
+// $('a').on('click', function (e) {
+//   const btnId = e.target.id
+
+//   switch (btnId) {
+//     case 'arr-length':
+//       findArrLength();
+//       break;
+//     case 'is-arr':
+//       checkIfIsArray();
+//       break;
+//     case 'get-single-value':
+//       getSingleValue();
+//       break;
+//     case 'insert-into':
+//       insertInto();
+//       break;
+//     case 'index-of':
+//       findIndexOf();
+//       break;
+//     case 'add-onto-end':
+//       addToEnd();
+//       break;
+//     default:
+//       break;
+//   }
+// });
